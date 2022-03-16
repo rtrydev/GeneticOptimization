e3edf463ec40697d4d2e9285b87fa5d5c75cb9a8
rtrydev <r.kulka44@gmail.com>
2022-03-16 21:01:08 +0100

    fix crashing on large datasets

--- GeneticOptimization/Files/FileWriter.cs
+++ GeneticOptimization/Files/FileWriter.cs
@@ -9,12 +9,20 @@ public class FileWriter
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 0; i < Matrix.Length - 1; i++)
+        /*for (int i = 0; i < Matrix.Length - 1; i++)
             stringBuilder.AppendLine(string.Join(" ", Matrix[i]));
-        stringBuilder.Append(string.Join(" ", Matrix[Matrix.Length - 1]));
+        stringBuilder.Append(string.Join(" ", Matrix[Matrix.Length - 1]));*/
 
         if(File.Exists(fileName)) return;
-        File.WriteAllText(fileName, stringBuilder.ToString());
+        using (var streamWriter = new StreamWriter(fileName))
+        {
+            for (int i = 0; i < Matrix.Length - 1; i++)
+            {
+                streamWriter.WriteLine(string.Join(" ", Matrix[i]));
+            }
+            streamWriter.Write(string.Join(" ", Matrix[^1]));
+        }
+        //File.WriteAllText(fileName, stringBuilder.ToString());
 
     }
 }
